36af25d054ac0f3887d77a4bae170b09b0a0589d
pbmean <kimjk@nextfoam.co.kr>
2022-06-20 14:08:22 +0900

    Writing operatingConditions and MRFProperties

---
+++ test/test_openfoam/test_operating_conditions.py
@@ -0,0 +1,27 @@
+import unittest
+
+from coredb import coredb
+from openfoam.operating_conditions import OperatingConditions
+
+
+class TestOperatingConditions(unittest.TestCase):
+    def setUp(self):
+        self.db = coredb.CoreDB()
+
+    def tearDown(self) -> None:
+        del coredb.CoreDB._instance
+
+    def testBuild(self):
+        region = 'testRegion_1'
+        zone = 'testZone_1'
+        pressure = '0'
+        self.db.addRegion(region)
+        self.db.addCellZone(region, zone)
+        self.db.setValue('.//operatingConditions/pressure', pressure)
+
+        content = OperatingConditions(region).asdict()
+        self.assertEqual(('Op [1 -1 -2 0 0 0 0]', pressure), content['Op'])
+
+
+if __name__ == '__main__':
+    unittest.main()
