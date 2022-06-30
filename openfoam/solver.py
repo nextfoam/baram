442d9abb01007dc44fa53ad561d33b28c0c64e27
jake <jakejiwon@gmail.com>
2022-06-30 13:52:33 +0900

    OpenFOAM Solver selection logic has been added

---
+++ openfoam/solver.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+import pandas as pd
+
+from coredb import coredb
+from resources import resource
+
+
+availableSolvers = pd.read_csv(resource.file('openfoam/solvers.csv'), header=0, index_col=0).transpose().to_dict()
+
+
+def findSolvers() -> list[str]:
+    db = coredb.CoreDB()
+
+    timeTransient   = db.getValue('.//general/timeTransient')
+    flowType        = db.getValue('.//general/flowType')
+    solverType      = db.getValue('.//general/solverType')
+    energyModel     = db.getValue('.//models/energyModels')
+    gravityDisabled = db.getAttribute('.//operatingConditions/gravity', 'disabled')
+    speciesModel    = db.getValue('.//models/speciesModels')
+    multiphaseModel = db.getValue('.//models/multiphaseModels/model')
+
+    pcs = []  # problem conditions
+
+    if solverType == 'densityBased':
+        pcs.append('solverTypeDensity')
+    else:  # pressure-based
+        pcs.append('solverTypePressure')
+
+    if timeTransient == 'true':
+        pcs.append('timeTransient')
+    else:  # steady
+        pcs.append('timeSteady')
+
+    if flowType == 'compressible':
+        pcs.append('flowTypeCompressible')
+    else:  # incompressible
+        pcs.append('flowTypeIncompressible')
+
+    if energyModel == 'off':
+        pcs.append('energyOff')
+    else:
+        pcs.append('energyOn')
+
+    if gravityDisabled == 'true':
+        pcs.append('gravityOff')
+    else:  # gravity enabled
+        pcs.append('gravityOn')
+
+    if speciesModel == 'off':
+        pcs.append('speciesSingle')
+    else:
+        pcs.append('speciesMultiple')
+
+    if multiphaseModel == 'off':
+        pcs.append('phaseSingle')
+    elif multiphaseModel == 'volumeOfFluid':
+        pcs.append('phaseVOF')
+    elif multiphaseModel == 'cavitation':
+        pcs.append('phaseCavitation')
+
+    numRegions = len(db.getRegions())
+    if numRegions > 1:  # multi-region
+        pcs.append('regionMultiple')
+    else:
+        pcs.append('regionSingle')
+
+    solvers = []
+    for sol, cap in availableSolvers.items():
+        if all([cap[p] for p in pcs]):
+            solvers.append(sol)
+            break
+
+    return solvers
---
+++ test/test_openfoam/test_solver.py
@@ -0,0 +1,62 @@
+import unittest
+
+from coredb import coredb
+import openfoam.solver
+
+
+class TestSolver(unittest.TestCase):
+    def setUp(self):
+        self.db = coredb.CoreDB()
+
+        self.region = 'testRegion_1'
+        zone = 'testZone_1'
+        self.db.addRegion(self.region)
+        self.db.addCellZone(self.region, zone)
+
+    def tearDown(self) -> None:
+        del coredb.CoreDB._instance
+
+    def testFindingPCNFoamForTransient(self):
+        self.db.setValue('.//general/timeTransient', 'true')
+        self.db.setValue('.//general/flowType', 'compressible')
+        self.db.setValue('.//general/solverType', 'pressureBased')
+        self.db.setAttribute('.//operatingConditions/gravity', 'disabled', 'true')
+        self.db.setValue('.//models/energyModels', 'on')
+        self.db.setValue('.//models/multiphaseModels/model', 'off')
+        self.db.setValue('.//models/speciesModels', 'off')
+
+        solvers = openfoam.solver.findSolvers()
+
+        self.assertEqual(1, len(solvers))
+        self.assertIn('PCNFoam', solvers)
+
+    def testFindingPCNFoamForSteady(self):
+        self.db.setValue('.//general/timeTransient', 'false')
+        self.db.setValue('.//general/flowType', 'compressible')
+        self.db.setValue('.//general/solverType', 'pressureBased')
+        self.db.setAttribute('.//operatingConditions/gravity', 'disabled', 'true')
+        self.db.setValue('.//models/energyModels', 'on')
+        self.db.setValue('.//models/multiphaseModels/model', 'off')
+        self.db.setValue('.//models/speciesModels', 'off')
+
+        solvers = openfoam.solver.findSolvers()
+
+        self.assertEqual(1, len(solvers))
+        self.assertIn('PCNFoam', solvers)
+
+    def testFindingPCNFoamForIncompressible(self):
+        self.db.setValue('.//general/timeTransient', 'false')
+        self.db.setValue('.//general/flowType', 'incompressible')
+        self.db.setValue('.//general/solverType', 'pressureBased')
+        self.db.setAttribute('.//operatingConditions/gravity', 'disabled', 'true')
+        self.db.setValue('.//models/energyModels', 'on')
+        self.db.setValue('.//models/multiphaseModels/model', 'off')
+        self.db.setValue('.//models/speciesModels', 'off')
+
+        solvers = openfoam.solver.findSolvers()
+
+        self.assertNotIn('PCNFoam', solvers)
+
+
+if __name__ == '__main__':
+    unittest.main()
