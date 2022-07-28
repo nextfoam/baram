7e2e53c5ccf80470c82b2561dfc16b5d6d6895e6
shpark <shpark@nextfoam.co.kr>
2022-07-29 01:37:52 +0900

    Process Information 수정

--- view/solution/process_information/process_information_page.py
+++ view/solution/process_information/process_information_page.py
@@ -7,7 +7,12 @@ from PySide6.QtWidgets import QWidget
 
 from coredb import coredb
 from coredb.project import Project, SolverStatus
+from openfoam.case_generator import CaseGenerator
 from openfoam.run import launchSolver
+from openfoam.system.fv_solution import FvSolution
+from openfoam.system.control_dict import ControlDict
+from openfoam.system.fv_schemes import FvSchemes
+from openfoam.system.fv_options import FvOptions
 import openfoam.solver
 from openfoam.file_system import FileSystem
 from .process_information_page_ui import Ui_ProcessInformationPage
@@ -39,6 +44,8 @@ class ProcessInformationPage(QWidget):
         Project.instance().statusChanged.connect(self._update)
 
     def _startCalculationClicked(self):
+        CaseGenerator().generateFiles()
+
         solvers = openfoam.solver.findSolvers()
         caseRoot = FileSystem.caseRoot()
         numCores = self._db.getValue('.//runCalculation/parallel/numberOfCores')
@@ -49,49 +56,59 @@ class ProcessInformationPage(QWidget):
         pid, createdTime = launchSolver(solvers[0], Path(caseRoot), int(numCores))
         Project.instance().setSolverProcess(pid, createdTime)
 
-        self._showSolverStatusRunning()
+        self._showStatusRunning()
 
     def _cancelCalculationClicked(self):
-        #
+        controlDict = ControlDict().build()
+        controlDict.asDict()['stopAt'] = 'noWriteNow'
+        controlDict.write()
 
-        #
-        self._showSolverStatusNone()
+        self._showStatusNone()
         self._ui.status.setText(self.tr('Canceled'))
 
     def _saveAndStopCalculationClicked(self):
+        controlDict = ControlDict().build()
+        controlDict.asDict()['stopAt'] = 'writeNow'
+        controlDict.write()
+
         self._ui.cancelCalculation.hide()
         self._ui.saveAndStopCalculation.setDisabled(True)
-        self._ui.updateConfiguration.setDisabled(True)
 
     def _updateConfigurationClicked(self):
-        ...
+        regions = self._db.getRegions()
+        for rname in regions:
+            FvSchemes(rname).build().write()
+            FvSolution(rname).build().write()
+            FvOptions(rname).build().write()
+        ControlDict().build().write()
 
     def _update(self, status):
         self._showStatus(status)
 
     def _showStatus(self, status):
         if status == SolverStatus.NONE:
-            self._showSolverStatusNone()
+            self._showStatusNone()
 
         elif status == SolverStatus.RUNNING:
-            self._showSolverStatusRunning()
+            self._showStatusRunning()
 
         elif status == SolverStatus.WAITING:
-            self._showSolverStatusWaiting()
+            self._showStatusWaiting()
         else:
             pass
 
-    def _showSolverStatusNone(self):
+    def _showStatusNone(self):
         self._ui.id.setText(self.tr('-'))
         self._ui.created.setText(self.tr('-'))
         self._ui.status.setText(self.tr('Not Running'))
 
         self._ui.startCalculation.show()
+        self._ui.startCalculation.setEnabled(True)
         self._ui.cancelCalculation.hide()
         self._ui.saveAndStopCalculation.setDisabled(True)
-        self._ui.updateConfiguration.setDisabled(True)
+        self._ui.updateConfiguration.setEnabled(True)
 
-    def _showSolverStatusRunning(self):
+    def _showStatusRunning(self):
         project = Project.instance()
 
         pid = project.pid
@@ -103,9 +120,13 @@ class ProcessInformationPage(QWidget):
 
         self._ui.startCalculation.hide()
         self._ui.cancelCalculation.show()
+        self._ui.cancelCalculation.setEnabled(True)
         self._ui.saveAndStopCalculation.setEnabled(True)
         self._ui.updateConfiguration.setEnabled(True)
 
-    def _showSolverStatusWaiting(self):
-        ...
+    def _showStatusWaiting(self):
+        self._ui.startCalculation.setDisabled(True)
+        self._ui.cancelCalculation.setDisabled(True)
+        self._ui.saveAndStopCalculation.setDisabled(True)
+        self._ui.updateConfiguration.setDisabled(True)
 
