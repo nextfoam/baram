98a78a9ae0b88cf0dd4d6014a8e4c4310cd93f1c
jake <jakejiwon@gmail.com>
2022-08-02 06:01:54 +0900

    Chart zoom has been added

--- view/main_window/chart_dock.py
+++ view/main_window/chart_dock.py
@@ -6,6 +6,8 @@ from pathlib import Path
 from PySide6.QtWidgets import QVBoxLayout, QWidget, QTextBrowser
 from PySide6.QtCore import Qt
 
+import numpy as np
+
 from matplotlib.backends.qt_compat import QtWidgets
 from matplotlib.backends.backend_qtagg import (
     FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
@@ -30,17 +32,16 @@ class ChartDock(TabifiedDock):
 
         layout = QtWidgets.QVBoxLayout(self._widget)
 
-        self.static_canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        self._canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        self._canvas.mpl_connect('scroll_event', self.onScroll)
         # Ideally one would use self.addToolBar here, but it is slightly
         # incompatible between PyQt6 and other bindings, so we just add the
         # toolbar as a plain widget instead.
-        layout.addWidget(NavigationToolbar(self.static_canvas, self))
-        layout.addWidget(self.static_canvas)
+        #layout.addWidget(NavigationToolbar(self.canvas, self))
+        layout.addWidget(self._canvas)
 
-        self._static_ax = self.static_canvas.figure.subplots()
-        self._static_ax.set_yscale('log')
-
-        self.lines1 = None
+        self._ax = self._canvas.figure.subplots()
+        self._ax.set_yscale('log')
 
         self.solverInfoManager = getSolverInfoManager(Path('./multiRegionHeater').resolve())
 
@@ -53,27 +54,36 @@ class ChartDock(TabifiedDock):
 
     def updated(self, data):
         for df in data:
-            print('output ')
             columns = list(filter(lambda x: x.endswith('_initial'),
-                             df.columns.values.tolist()))
-            print('output ')
+                                  df.columns.values.tolist()))
+
             timeMin = df.first_valid_index()
             timeMax = df.last_valid_index()
-            print(f'time range ({timeMin}, {timeMax})')
 
             d = df.reset_index()  # "Time" is back to a column to serve as X value
 
             for c in columns:
                 if c not in self._lines:
-                    print('newLine '+c)
-                    self._lines[c], = self._static_ax.plot('Time', c, '', label=c, data=d)
+                    self._lines[c], = self._ax.plot('Time', c, '', label=c[:-8], data=d)
                 else:
-                    print('updateLine ' + c)
                     self._lines[c].set_data(d[['Time', c]].to_numpy().transpose())
 
-        self._static_ax.set_xlim(None, timeMax+1)
-        self._static_ax.legend()
-        self.static_canvas.draw()
+        self._ax.set_xlim(None, timeMax)
+        self._ax.legend()
+        self._canvas.draw()
+
+    def onScroll(self, event):
+        print(f'mouse event {event.step}')
+        minX, maxX = self._ax.get_xlim()
+        scale = np.power(1.05, -event.step)
+        print(f'scale {scale}')
+        left = maxX - (maxX-minX) * scale
+        self._ax.set_xlim([left, maxX])
+        self._ax.relim(visible_only=True)
+        self._ax.set_autoscaley_on(True)
+        self._ax.autoscale_view(scalex=False, scaley=True)
+
+        self._canvas.draw()  # force re-draw the next time the GUI refreshes
 
 
 
