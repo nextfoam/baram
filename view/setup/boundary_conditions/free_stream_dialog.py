d4fd2bcc9f8443dff6abda815f5d7fe61748e2de
jake <jakejiwon@gmail.com>
2022-09-16 15:52:32 +0900

    freestrem boundary condition requires Temperature input

--- view/setup/boundary_conditions/free_stream_dialog.py
+++ view/setup/boundary_conditions/free_stream_dialog.py
@@ -5,10 +5,12 @@ from PySide6.QtWidgets import QMessageBox
 
 from coredb import coredb
 from coredb.coredb_writer import CoreDBWriter
+from coredb.models_db import ModelsDB
 from coredb.boundary_db import BoundaryDB
 from view.widgets.resizable_dialog import ResizableDialog
 from .free_stream_dialog_ui import Ui_FreeStreamDialog
 from .turbulence_model_helper import TurbulenceModelHelper
+from .temperature_widget import TemperatureWidget
 
 
 class FreeStreamDialog(ResizableDialog):
@@ -22,9 +24,14 @@ class FreeStreamDialog(ResizableDialog):
         self._db = coredb.CoreDB()
         self._xpath = BoundaryDB.getXPath(bcid)
         self._turbulenceWidget = TurbulenceModelHelper.createWidget(self._xpath)
+        self._temperatureWidget = None
 
+        layout = self._ui.dialogContents.layout()
         if self._turbulenceWidget:
-            self._ui.dialogContents.layout().addWidget(self._turbulenceWidget)
+            layout.addWidget(self._turbulenceWidget)
+        if ModelsDB.isEnergyModelOn():
+            self._temperatureWidget = TemperatureWidget(self._xpath, bcid)
+            layout.addWidget(self._temperatureWidget)
 
         self._load()
 
@@ -39,6 +46,9 @@ class FreeStreamDialog(ResizableDialog):
 
         if self._turbulenceWidget:
             self._turbulenceWidget.appendToWriter(writer)
+        if self._temperatureWidget:
+            if not self._temperatureWidget.appendToWriter(writer):
+                return
 
         errorCount = writer.write()
         if errorCount > 0:
@@ -56,3 +66,5 @@ class FreeStreamDialog(ResizableDialog):
 
         if self._turbulenceWidget:
             self._turbulenceWidget.load()
+        if self._temperatureWidget:
+            self._temperatureWidget.load()
