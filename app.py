173b1d45990240b582b21c794f366719d795531b
jake <jakejiwon@gmail.com>
2022-11-25 13:55:40 +0900

    Issue #179, path of files change when packaged

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 from pathlib import Path
 
-APP_PATH = Path(__file__).parent.resolve()
+if getattr(sys, 'frozen', False):
+    APP_PATH = Path(sys.executable).parent.resolve()
+else:
+    APP_PATH = Path(__file__).parent.resolve()
 
 
 class App:
