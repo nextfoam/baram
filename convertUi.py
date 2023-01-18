4c6b1927fa4ea5c4b61aa72c98c453a0efe10bf4
jake <jakejiwon@gmail.com>
2023-01-18 17:50:03 +0900

    A feature to check file timestamp, like Make, has been added

--- convertUi.py
+++ convertUi.py
@@ -9,19 +9,32 @@ import subprocess
 print('>> Convert Translation Files')
 for ts in Path('resources', 'locale').glob('baram_*.ts'):
     qm = ts.with_suffix('.qm')
-    subprocess.run(['pyside6-lrelease', ts, '-qm', qm])
+    if qm.is_file() and qm.stat().st_mtime >= ts.stat().st_mtime:
+        print(f'  Skipping...   {ts} -> {qm}, Already Up-to-date')
+    else:
+        print(f'  Converting... {ts} -> {qm}')
+        subprocess.run(['pyside6-lrelease', ts, '-qm', qm])
 
 
 # Convert QResource File
+target = Path('resource_rc.py')
+source = Path('resource.qrc')
 print('>> Convert QResource File')
-print(f'  Converting... resource.qrc -> resource_rc.py')
-subprocess.run(['pyside6-rcc', 'resource.qrc', '-o', 'resource_rc.py'])
+if target.is_file() and target.stat().st_mtime >= source.stat().st_mtime:
+    print(f'  Skipping...   {source} -> {target}, Already Up-to-date')
+else:
+    print(f'  Converting... {source} -> {target}')
+    subprocess.run(['pyside6-rcc', source, '-o', target])
 
 
 # Convert QT Designer Files
 print('\n>> Convert QT Designer Files')
 paths = list(Path('view').glob('**/*.ui'))  # Convert to 'list' to get the length of it
 totalNum = len(paths)
-for i, p in enumerate(paths):
-    print(f'  [{i+1}/{totalNum}] Converting... {p.name} -> {p.stem}_ui.py')
-    subprocess.run(['pyside6-uic',  p, '-o', f'{p.parent/p.stem}_ui.py'])
+for i, source in enumerate(paths):
+    target = source.parent / (source.stem + '_ui.py')
+    if target.is_file() and target.stat().st_mtime >= source.stat().st_mtime:
+        print(f'  [{i+1}/{totalNum}] Skipping...   {source.name} -> {source.stem}_ui.py, Already Up-to-date')
+    else:
+        print(f'  [{i+1}/{totalNum}] Converting... {source.name} -> {source.stem}_ui.py')
+        subprocess.run(['pyside6-uic', source, '-o', target])
