746430890657486049cf921a5318017e4739d6d5
pbmean <kimjk@nextfoam.co.kr>
2023-02-10 13:28:34 +0900

    Issue #234 energy model이 on일 때에만 fvOptions 파일에 limitT 항목을 작성하도록 수정

--- openfoam/system/fv_options.py
+++ openfoam/system/fv_options.py
@@ -21,15 +21,19 @@ class FvOptions(DictionaryFile):
     def build(self):
         if self._data is not None:
             return self
-        self._data = {
-            'limitT': {
-                'type': 'limitTemperature',
-                'active': 'yes',
-                'selectionMode': 'all',
-                'min': self._db.getValue('.//numericalConditions/advanced/limits/minimumStaticTemperature'),
-                'max': self._db.getValue('.//numericalConditions/advanced/limits/maximumStaticTemperature')
+
+        self._data = {}
+
+        if ModelsDB.isEnergyModelOn():
+            self._data = {
+                'limitT': {
+                    'type': 'limitTemperature',
+                    'active': 'no',
+                    'selectionMode': 'all',
+                    'min': self._db.getValue('.//numericalConditions/advanced/limits/minimumStaticTemperature'),
+                    'max': self._db.getValue('.//numericalConditions/advanced/limits/maximumStaticTemperature')
+                }
             }
-        }
 
         cellZones = self._db.getCellZones(self._rname)
         for czid, czname in cellZones:
