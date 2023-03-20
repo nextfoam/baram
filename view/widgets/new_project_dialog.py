98e6677b0e49e85057e7e33f087a0ce9618fc03f
pbmean <kimjk@nextfoam.co.kr>
2023-03-20 14:41:13 +0900

    앱 시작 코드 정리 (프로젝트 생성) 누락 코드 커밋

---
+++ view/widgets/new_project_dialog.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+from PySide6.QtWidgets import QDialog, QFileDialog, QDialogButtonBox
+
+from coredb.app_settings import AppSettings
+from .new_project_dialog_ui import Ui_NewProjectDialog
+
+
+class NewProjectDialog(QDialog):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self._ui = Ui_NewProjectDialog()
+        self._ui.setupUi(self)
+
+        self._complete = False
+        self._locationParent = Path(AppSettings.getRecentLocation()).resolve()
+        self._updateProjectLocation()
+
+        self._dialog = None
+
+        self._connectSignalsSlots()
+
+    def projectLocation(self):
+        return Path(self._ui.projectLocation.text())
+
+    def _connectSignalsSlots(self):
+        self._ui.projectName.textChanged.connect(self._updateProjectLocation)
+        self._ui.select.clicked.connect(self._selectLocation)
+
+    def _selectLocation(self):
+        self._dialog = QFileDialog(self, self.tr('Select Location'), str(self._locationParent))
+        self._dialog.setFileMode(QFileDialog.FileMode.Directory)
+        self._dialog.fileSelected.connect(self._locationParentSelected)
+        self._dialog.open()
+
+    def _updateProjectLocation(self):
+        self._ui.projectLocation.setText(str(self._locationParent / self._ui.projectName.text()))
+
+        complete = False
+
+        if not self._locationParent.exists():
+            self._ui.validationMessage.setText(self.tr(f'{self._locationParent} is not a directory.'))
+        elif not self._ui.projectName.text():
+            self._ui.validationMessage.clear()
+        elif Path(self._ui.projectLocation.text()).exists():
+            self._ui.validationMessage.setText(self.tr(f'{self._ui.projectLocation.text()} already exists.'))
+        else:
+            self._ui.validationMessage.clear()
+            complete = True
+
+        self._ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(complete)
+
+    def _locationParentSelected(self, dir):
+        self._locationParent = Path(dir).resolve()
+        self._updateProjectLocation()
