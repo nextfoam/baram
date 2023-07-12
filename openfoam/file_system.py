8de2da65aafa46403a6185ad9401161d39cd9eaa
pbmean <kimjk@nextfoam.co.kr>
2023-07-12 17:16:20 +0900

    case 디렉토리 생성할 때 빈 하위 디렉토리 생성

--- openfoam/file_system.py
+++ openfoam/file_system.py
@@ -7,18 +7,26 @@ import asyncio
 from libbaram import utils
 
 
+def makeDir(parent, directory):
+    path = parent / directory
+    path.mkdir(parents=True, exist_ok=True)
+    return path
+
+
 class FileSystem:
     CASE_DIRECTORY_NAME = 'case'
     CONSTANT_DIRECTORY_NAME = 'constant'
     BOUNDARY_CONDITIONS_DIRECTORY_NAME = '0'
     SYSTEM_DIRECTORY_NAME = 'system'
-    POLY_MESH_DIRECTORY = CONSTANT_DIRECTORY_NAME + '/polyMesh'
     REGION_PROPERTIES_FILE_NAME = 'regionProperties'
     FOAM_FILE_NAME = 'baram.foam'
-    TRI_SURFACE_DIRECTORY = CONSTANT_DIRECTORY_NAME + '/triSurface'
+    TRI_SURFACE_DIRECTORY_NAME = 'triSurface'
+    POLY_MESH_DIRECTORY_NAME = 'polyMesh'
 
     def __init__(self, path):
         self._casePath = None
+        self._constantPath = None
+        self._triSurfacePath = None
 
         self._setCaseRoot(path / self.CASE_DIRECTORY_NAME)
 
@@ -31,6 +39,9 @@ class FileSystem:
 
         shutil.copytree(src, self._casePath)
 
+        self._constantPath = makeDir(self._casePath, self.CONSTANT_DIRECTORY_NAME)
+        self._triSurfacePath = makeDir(self._constantPath, self.TRI_SURFACE_DIRECTORY_NAME)
+
     def triSurfacePath(self):
         return self._triSurfacePath
 
@@ -39,4 +50,5 @@ class FileSystem:
 
     def _setCaseRoot(self, path):
         self._casePath = path
-        self._triSurfacePath = self._casePath / self.TRI_SURFACE_DIRECTORY
+        self._constantPath = self._casePath / self.CONSTANT_DIRECTORY_NAME
+        self._triSurfacePath = self._constantPath / self.TRI_SURFACE_DIRECTORY_NAME
