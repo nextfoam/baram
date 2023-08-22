5434598298480ebc27ad650d1e479efd178b7702
jake <jakejiwon@gmail.com>
2023-08-22 11:15:36 +0900

    Issue #281, Residual chart refactored

--- view/main_window/chart_dock.py
+++ view/main_window/chart_dock.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import typing
 from pathlib import Path
 
 import pandas as pd
@@ -13,6 +13,7 @@ from matplotlib.backends.qt_compat import QtWidgets
 from matplotlib.backends.backend_qtagg import (
     FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
 from matplotlib.figure import Figure
+from matplotlib.lines import Line2D
 import matplotlib.ticker as ticker
 
 from openfoam.solver_info_manager import SolverInfoManager
@@ -20,10 +21,10 @@ from openfoam.file_system import FileSystem
 from .tabified_dock import TabifiedDock
 from coredb import coredb
 from coredb.general_db import GeneralDB
-from coredb.run_calculation_db import RunCalculationDB
+from coredb.run_calculation_db import RunCalculationDB, TimeSteppingMethod
 from coredb.project import Project, SolverStatus
 
-END_MARGIN = 0.05  # 5% margin between line end and right axis
+SIDE_MARGIN = 0.05  # 5% margin on left and right
 
 
 class ChartDock(TabifiedDock):
@@ -33,9 +34,8 @@ class ChartDock(TabifiedDock):
         self._main_window = parent
 
         self._data = None
-        self._timeMax = None
-        self._timeMin = None
-        self._lines = {}
+
+        self._lines: typing.Dict[str, Line2D] = {}
 
         self.setAllowedAreas(Qt.RightDockWidgetArea)
 
@@ -64,8 +64,6 @@ class ChartDock(TabifiedDock):
         self._translate()
 
     def startDrawing(self):
-        self._timeMax = None
-        self._timeMin = None
         self.solverInfoManager.startCollecting(Path(FileSystem.caseRoot()).resolve(), coredb.CoreDB().getRegions())
 
     def stopDrawing(self):
@@ -90,10 +88,7 @@ class ChartDock(TabifiedDock):
         self._data = data
 
         d = data.reset_index()  # "Time" is back to a column to serve as X value in numpy transpose below
-        #
-        # columns = list(filter(lambda x: x.endswith('_initial'),
-        #                       data.columns.values.tolist()))
-        #
+
         for c in data.columns.values.tolist():
             if c not in self._lines:
                 self._lines[c], = self._axes.plot('Time', c, '', label=c, data=d)
@@ -101,72 +96,43 @@ class ChartDock(TabifiedDock):
             else:
                 self._lines[c].set_data(d[['Time', c]].to_numpy().transpose())
 
-        timeMax = float(data.last_valid_index())
-        timeMin = float(data.first_valid_index())
-
-        minX, maxX = self._axes.get_xlim()
-        chartWidth = (maxX - minX)
-
-        if self._timeMax is None:  # new drawing
-            dataWidth = chartWidth * (1 - 2 * END_MARGIN)
-            if (timeMax - timeMin) > dataWidth:
-                maxX = timeMax + chartWidth * END_MARGIN
-                minX = maxX - chartWidth
-            else:
-                minX = timeMin - END_MARGIN
-                maxX = minX + chartWidth
-        else:
-            if timeMax + chartWidth * END_MARGIN < maxX:
-                pass  # keep the range as is
-            else:
-                maxX = timeMax + chartWidth * END_MARGIN
-                minX = maxX - chartWidth
-
-        self._timeMax = timeMax
-        self._timeMin = timeMin
-
-        self._axes.set_xlim([minX, maxX])
-
-        self._adjustYRange(data, minX, maxX)
-
         legend = self._axes.legend()
         for h in legend.legendHandles:
             h.set_linewidth(1.6)
 
-        self._canvas.draw()
-        # self._canvas.draw_idle()
+        self._updateChart(1.0)
 
     def onScroll(self, event):
-        if self._timeMax is None:
+        scale = np.power(1.05, -event.step)
+        self._updateChart(scale)
+
+    def _updateChart(self, scale: float):
+        if self._data is None:
             return
 
-        minX, maxX = self._axes.get_xlim()
-        width = maxX - minX
+        timeMin = float(self._data.first_valid_index())
+        timeMax = float(self._data.last_valid_index())
 
-        # this margin will be kept without change
-        margin = (maxX - self._timeMax) / width
+        dataWidth = timeMax - timeMin
 
-        scale = np.power(1.05, -event.step)
+        # chartWidth is effective width for lines excluding side margins
+        left, right = self._axes.get_xlim()
+        margin = (right - left) * SIDE_MARGIN
+        chartWidth = ((right - left) - 2 * margin)
 
-        newWidth = width * scale
+        margin *= scale
+        chartWidth *= scale
 
-        if scale < 1:  # Zoom in
-            # if self._timeMax + width * END_MARGIN >= maxX:
-            if minX > self._timeMin or maxX <= self._timeMax + width * END_MARGIN:
-                maxX = self._timeMax + newWidth * margin
-                minX = self._timeMax - newWidth * (1 - margin)
-            else:
-                maxX = minX + newWidth
-        else:  # Zoom out
-            if minX >= self._timeMin:
-                maxX = self._timeMax + newWidth * margin
-                minX = self._timeMax - newWidth * (1 - margin)
-            else:
-                maxX = minX + newWidth
+        if dataWidth < chartWidth:
+            minX = timeMin
+            maxX = minX + chartWidth
+        else:
+            maxX = timeMax
+            minX = maxX - chartWidth
 
-        self._axes.set_xlim([minX, maxX])
+        self._axes.set_xlim([minX-margin, maxX+margin])
 
-        self._adjustYRange(self._data, minX, maxX)
+        self._adjustYRange(minX, maxX)
 
         self._canvas.draw()  # force re-draw the next time the GUI refreshes
         # self._canvas.draw_idle()
@@ -176,22 +142,15 @@ class ChartDock(TabifiedDock):
     def _translate(self):
         self.setWindowTitle(self.tr("Residuals"))
 
-    def _adjustYRange(self, data: [pd.DataFrame], minX: float, maxX: float):
-        minY = None
-        maxY = None
+    def _adjustYRange(self, minX: float, maxX: float):
+        data = self._data
 
-        # columns = list(filter(lambda x: x.endswith('_initial'),
-        #                       data.columns.values.tolist()))
-        # d = data[(data.index >= minX) & (data.index <= maxX)][columns]
         d = data[(data.index >= minX) & (data.index <= maxX)]
-        minimum = d[d > 0].min().min()  # Residual value of "0" has been shown once
-        if minY is None or minY > minimum:
-            minY = minimum
-
-        if maxY is None or maxY < d.max().max():
-            maxY = d.max().max()
+        minY = d[d > 0].min().min()  # Residual value of "0" has been shown once
+        maxY = d.max().max()
 
         minY = minY / 10  # margin in log scale
+
         if maxY < 0.1:
             maxY = maxY * 10  # margin in log scale
         else:
@@ -203,8 +162,6 @@ class ChartDock(TabifiedDock):
         self._axes.cla()
 
         self._data = None
-        self._timeMax = None
-        self._timeMin = None
         self._lines = {}
 
         self._axes.grid(alpha=0.6, linestyle='--')
@@ -213,18 +170,22 @@ class ChartDock(TabifiedDock):
         self._axes.set_yscale('log')
 
         if GeneralDB.isTimeTransient():
-            # 10% of total case time
-            endTime = float(coredb.CoreDB().getValue(RunCalculationDB.RUN_CALCULATION_XPATH + '/runConditions/endTime'))
-            maxX = endTime / 10
-        else:
-            # 10% of total iteration count or iteration count if it is less than MIN_COUNT
-            MIN_COUNT = 100
-            count = int(coredb.CoreDB().getValue(RunCalculationDB.RUN_CALCULATION_XPATH + '/runConditions/numberOfIterations'))
-            if count < MIN_COUNT:
-                maxX = count
+            timeSteppingMethod = coredb.CoreDB().getValue(RunCalculationDB.RUN_CALCULATION_XPATH + '/runConditions/timeSteppingMethod')
+            if timeSteppingMethod == TimeSteppingMethod.FIXED.value:
+                # 50 Residual points
+                timeStep = float(coredb.CoreDB().getValue(RunCalculationDB.RUN_CALCULATION_XPATH + '/runConditions/timeStepSize'))
+                chartWidth = timeStep * 50
             else:
-                maxX = MIN_COUNT + count / 10
+                # 10% of total case time
+                endTime = float(coredb.CoreDB().getValue(RunCalculationDB.RUN_CALCULATION_XPATH + '/runConditions/endTime'))
+                chartWidth = endTime / 10
+        else:
+            # 50 Residual points
+            chartWidth = 50
 
-        self._axes.set_xlim([-(maxX * END_MARGIN), maxX])
+        margin = chartWidth * SIDE_MARGIN
+        minX = -margin
+        maxX = chartWidth + margin
+        self._axes.set_xlim([minX, maxX])
 
         self._canvas.draw()
