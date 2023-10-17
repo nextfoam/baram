e2d894c8bf0a5bec6ee8dcf253363639db7e121b
Jake Yun <jakejiwon@gmail.com>
2023-10-17 12:43:15 +0900

    convenient script to run the applications

---
+++ baramMesh.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SOURCE=${BASH_SOURCE[0]}
+
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+    DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+    SOURCE=$(readlink "$SOURCE")
+    [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+
+cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1
+
+source venv/bin/activate
+
+python -m baramMesh.main
