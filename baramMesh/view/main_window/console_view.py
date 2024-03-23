6802aa8704eca515a8e1318c4adef101c2cfbbb8
jake <jakejiwon@gmail.com>
2024-03-23 15:40:31 +0900

    Issue baram#476 Fixed width font configuration for baramMesh console

--- baramMesh/view/main_window/console_view.py
+++ baramMesh/view/main_window/console_view.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from PySide6.QtCore import QObject
+from PySide6.QtGui import QFontDatabase
 
 from PySide6.QtWidgets import QPlainTextEdit
 
@@ -9,7 +10,12 @@ from PySide6.QtWidgets import QPlainTextEdit
 class ConsoleView(QObject):
     def __init__(self, ui):
         super().__init__()
-        self._view:QPlainTextEdit = ui.console
+        self._view: QPlainTextEdit = ui.console
+
+        charFormat = self._view.currentCharFormat()
+        fixedFont = QFontDatabase.systemFont(QFontDatabase.FixedFont)
+        charFormat.setFont(fixedFont)
+        self._view.setCurrentCharFormat(charFormat)
 
     def clear(self):
         self._view.clear()
