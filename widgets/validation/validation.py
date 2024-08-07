0811a25c2a9b24e8a98ec1d372c4393516595845
pbmean <kimjk@nextfoam.co.kr>
2024-08-07 20:48:24 +0900

    validation 모듈 추가

---
+++ widgets/validation/validation.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PySide6.QtCore import QObject
+
+
+class Validator(QObject):
+    def __init__(self):
+        super().__init__()
+
+    def validate(self):
+        raise NotImplementedError
+
+
+class FormValidator(Validator):
+    def __init__(self):
+        super().__init__()
+
+        self._required = []
+        self._validations = []
+
+    def addRequiredValidation(self, edit, name):
+        self._required.append((edit, name))
+
+    def addCustomValidation(self, validator):
+        self._validations.append(validator)
+
+    def validate(self):
+        for edit, name in self._required:
+            if not edit.text().strip():
+                return False, self.tr('{} is required.'.format(name))
+
+        for v in self._validations:
+            valid, msg = v.validate()
+            if not valid:
+                return False, msg
+
+        return True, None
