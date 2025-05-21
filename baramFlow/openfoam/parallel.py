d593fc9d275f9903ee7fe88adfef6fec1251ed88
jake <jakejiwon@gmail.com>
2025-05-21 17:07:31 +0900

    Issue baram#634 Legacy code for compatibiliby removed

--- baramFlow/openfoam/parallel.py
+++ baramFlow/openfoam/parallel.py
@@ -9,20 +9,11 @@ from baramFlow.coredb.project import Project
 
 def getNP() -> int:
     numCoresStr = Project.instance().np
-    if numCoresStr is None:  # ToDo: For compatibility. Remove this code block after 20240601
-        numCoresStr = coredb.CoreDB().getValue('/runCalculation/parallel/numberOfCores')
-
     return int(numCoresStr)
 
 
 def getParallelType() -> ParallelType:
     ptypeStr = Project.instance().pType
-    if ptypeStr is None:  # ToDo: For compatibility. Remove this code block after 20240601
-        if coredb.CoreDB().getValue('/runCalculation/parallel/localhost') == 'true':
-            return ParallelType.LOCAL_MACHINE
-        else:
-            return ParallelType.CLUSTER
-        
     return ParallelType(ptypeStr) if isinstance(ptypeStr, int) else ParallelType[ptypeStr]
 
 
