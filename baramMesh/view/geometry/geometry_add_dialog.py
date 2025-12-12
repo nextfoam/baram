e3ada23259793eb2979723fa05f4ea881bc6bcc3
pbmean <pbmeanf@gmail.com>
2025-12-12 18:47:25 +0900

    Issue baram#325 Geometry transform 기능 추가

--- baramMesh/view/geometry/geometry_add_dialog.py
+++ baramMesh/view/geometry/geometry_add_dialog.py
@@ -1,18 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from PySide6.QtWidgets import QDialog, QDialogButtonBox
+from PySide6.QtCore import Signal
 
+from PySide6.QtWidgets import QDialog
+
+from widgets.enum_button_group import EnumButtonGroup
+
+from baramMesh.db.configurations_schema import Shape
 from .geometry_add_dialog_ui import Ui_GeometryAddDialog
 
 
 class GeometryAddDialog(QDialog):
+    shapeSelected = Signal(Shape)
+
     def __init__(self, parent):
         super().__init__(parent)
         self._ui = Ui_GeometryAddDialog()
         self._ui.setupUi(self)
 
-        self._ui.buttonBox.button(QDialogButtonBox.StandardButton.Ok).setText(self.tr('Next'))
+        self._radios = EnumButtonGroup()
+        self._radios.addEnumButton(self._ui.hex,        Shape.HEX)
+        self._radios.addEnumButton(self._ui.cylinder,   Shape.CYLINDER)
+        self._radios.addEnumButton(self._ui.sphere,     Shape.SPHERE)
+        self._radios.addEnumButton(self._ui.hex6,       Shape.HEX6)
 
-    def selectedShape(self):
-        return self._ui.shapeRadios.checkedButton().objectName()
+        self._connectSignalsSlots()
+
+    def _connectSignalsSlots(self):
+        self._ui.next.clicked.connect(self._onAccept)
+
+    def _onAccept(self):
+        self.shapeSelected.emit(self._radios.checkedData())
+
+        self.accept()
