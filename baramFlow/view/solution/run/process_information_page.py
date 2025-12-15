bed3dc9f546c1acdf3766238c1ad8baec05bfada
pbmean <pbmeanf@gmail.com>
2025-12-15 12:09:51 +0900

    Issue baram#754 POD 기능 추가로 발생한 중복 코드 제거

--- baramFlow/view/solution/run/process_information_page.py
+++ baramFlow/view/solution/run/process_information_page.py
@@ -72,7 +72,6 @@ class ProcessInformationPage(ContentPage):
         self._ui.calculation.setMinimumWidth(self.parent().width() - 30)
         self._connectSignalsSlots()
 
-        self._batchCaseList.load()
         self._setRunningMode(RunningMode.LIVE_RUNNING_MODE)
 
     def showEvent(self, ev):
